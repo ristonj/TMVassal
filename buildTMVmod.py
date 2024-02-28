c7e173fb92a00d92d692cc520cd110f51b689bf7
John Riston <ristonj@gmail.com>
2024-02-28 11:19:06 -0500

    Refactored XML file to be able to handle arbitrary units.

--- buildTMVmod.py
+++ buildTMVmod.py
@@ -2,24 +2,26 @@ import os
 import xml.etree.ElementTree as ET
 import zipfile
 
-def buildInfantryNodes(xml_file: str, gpid: int) -> tuple[int, list[ET.Element]]:
+from models.tokenCommonData import TokenCommonData
+
+def buildTokens(xml_file: str, gpid: int) -> tuple[int, list[ET.Element]]:
     tree = ET.parse(xml_file)
     root = tree.getroot()
-    infantryParent = root.find("./tokenGroupParent").attrib["name"]
-    infantryChild = root.find("./tokenGroupChild").attrib["name"]
-    infantryTemplates = root.find("./powerInfantry")
-    infantryPowers = infantryTemplates.find("./powers")
-    infantryGroups = []
-    xStart = infantryTemplates.attrib["xStart"]
-    xInc = infantryTemplates.attrib["xInc"]
-    yCurrent = infantryTemplates.attrib["yStart"]
-    yInc = infantryTemplates.attrib["yInc"]
-    for infantryPowerTokens in infantryPowers.findall("./power"):
-        xCurrent = xStart
-        for token in infantryPowerTokens.findall("./token"):
-            tokenName = "{0} {1} infantry".format(token.attrib["strength"], infantryPowerTokens.attrib["name"])
-            infantryGroup = ET.Element(
-                infantryParent,
+    powers = root.find("./powers")
+    groups = []
+    tokenCommonData = getTokenCommonData(root)
+    yCurrent = tokenCommonData.yStart
+    for tokens in powers.findall("./power"):
+        xCurrent = tokenCommonData.xStart
+        for token in tokens.findall("./token"):
+            tokenType = root.find("./tokenTypes/tokenType[@name='{0}']".format(token.attrib["type"]))
+            powerName = tokens.attrib["name"]
+            strength = ""
+            if "strength" in token.attrib.keys():
+                strength = token.attrib["strength"]
+            tokenName = tokenType.attrib["nameTemplate"].format(strength=strength, power=powerName)
+            group = ET.Element(
+                tokenCommonData.parent,
                 attrib={
                     "name": tokenName,
                     "owningBoard": root.find("owningBoard").attrib["name"],
@@ -30,36 +32,37 @@ def buildInfantryNodes(xml_file: str, gpid: int) -> tuple[int, list[ET.Element]]
             )
             for _ in range(int(token.attrib["numTokens"])):
                 node = ET.SubElement(
-                    infantryGroup,
-                    infantryChild,
+                    group,
+                    tokenCommonData.child,
                     attrib={
                         "entryName": tokenName,
                         "gpid": str(gpid),
-                        "height": root.find("./tokenDimensions/dimension[@type]").attrib["height"],
-                        "width": root.find("./tokenDimensions/dimension[@type]").attrib["width"]
+                        "height": root.find("./tokenDimensions/dimension[@type='{0}']".format(tokenType.attrib["dimension"])).attrib["height"],
+                        "width": root.find("./tokenDimensions/dimension[@type='{0}']".format(tokenType.attrib["dimension"])).attrib["width"]
                     }
                 )
-                node.text=infantryTemplates.find("./tokenText").text.format(
+                node.text=root.find("./tokenText").text.format(
                             x=xCurrent,
                             y=yCurrent,
                             prototype="Land\\/Naval Units",
-                            backCommand="Militia",
-                            frontCommand="Regular",
-                            frontImage=infantryPowerTokens.attrib["name"].replace(" ", "").lower() + token.attrib["strength"] + "reg.png",
-                            backImage=infantryPowerTokens.attrib["name"].replace(" ", "").lower() + token.attrib["strength"] + "mil.png",
-                            layerNames="1 Nasrid regular,1 Nasrid militia",
+                            backCommand=tokenType.attrib["backCommand"],
+                            frontCommand=tokenType.attrib["frontCommand"],
+                            frontImage=tokenType.attrib["frontImage"].format(power=powerName.replace(" ", "").lower(), strength=strength),
+                            backImage=tokenType.attrib["backImage"].format(power=powerName.replace(" ", "").lower(), strength=strength),
+                            layerNames=tokenType.attrib["layers"].format(power=powerName, strength=strength),
                             name=tokenName
                 )
+                print(node.text)
                 gpid += 1
-            infantryGroups.append(infantryGroup)
-            xCurrent = str(int(xCurrent) + int(xInc))
-        yCurrent = str(int(yCurrent) + int(yInc))
-        xCurrent = xStart
-    return gpid, infantryGroups
+            groups.append(group)
+            xCurrent = str(int(xCurrent) + int(tokenType.attrib["xInc"]))
+        yCurrent = str(int(yCurrent) + int(tokens.attrib["yInc"]))
+        xCurrent = tokenCommonData.xStart
+    return gpid, groups
 
 def createVmod():
     imagePath = "./images"
-    with zipfile.ZipFile("tmvip.vmod", "w") as vmodZip:
+    with zipfile.ZipFile("tmwip.vmod", "w") as vmodZip:
         vmodZip.write("buildFile.xml")
         vmodZip.write("moduledata")
         for root, dirs, files in os.walk(imagePath):
@@ -68,11 +71,21 @@ def createVmod():
                         os.path.relpath(os.path.join(root, file), 
                                         os.path.join(imagePath, '..')))
 
+def getTokenCommonData(root):
+    powers=root.find("./powers")
+    return TokenCommonData(
+        parent=root.find("./tokenGroupParent").attrib["name"],
+        child=root.find("./tokenGroupChild").attrib["name"],
+        tokenText=root.find("./tokenText").text,
+        xStart=powers.attrib["xStart"],
+        yStart=powers.attrib["yStart"]
+    )
+
 def main():
     tree = ET.parse("BaseTantoMonta.xml")
     root = tree.getroot()
     initGPID = int(root.attrib["nextPieceSlotId"])
-    nextID, infantryNodes = buildInfantryNodes("tokens.xml", initGPID)
+    nextID, infantryNodes = buildTokens("tokens.xml", initGPID)
     root.attrib["nextPieceSlotId"] = str(nextID)
     infantryRoot = root.find("./VASSAL.build.module.Map[@mapName='Land/Naval Units']")
     for node in infantryNodes:
---
+++ models/tokenCommonData.py
@@ -0,0 +1,7 @@
+class TokenCommonData:
+    def __init__(self, parent, child, tokenText, xStart, yStart):
+        self.parent = parent
+        self.child = child
+        self.tokenText = tokenText
+        self.xStart = xStart
+        self.yStart = yStart
