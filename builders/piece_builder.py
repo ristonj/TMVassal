3b10deea2390a2dc2b1c5383c650be4cb8207d60
John Riston <ristonj@gmail.com>
2024-06-09 01:21:53 -0400

    Added multi VP pieces and Anfa.

--- builders/piece_builder.py
+++ builders/piece_builder.py
@@ -31,11 +31,12 @@ class PieceBuilder:
             for piece in panel.findall("./singleSidedPiece"):
                 self._get_subelement(
                     panel_element,
-                    piece.attrib["name"]
-                ).text = tokenText.format(
-                    image=self._get_image_name(piece.attrib["name"]) + ".png",
-                    name=piece.attrib["name"],
-                    gpid=str(self.gpid)
+                    piece.attrib["name"],
+                    tokenText.format(
+                        image=self._get_image_name(piece.attrib["name"]) + ".png",
+                        name=piece.attrib["name"],
+                        gpid=str(self.gpid)
+                    )
                 )
                 self.gpid += 1
             piece_list.append(panel_element)
@@ -60,21 +61,56 @@ class PieceBuilder:
                 "scale": "1.0",
                 "width": vp_node.attrib["width"]
             })
-        for vp_piece in vp_node.findall("./singleVP"):
-            self._get_subelement(
-                panel,
-                vp_piece.attrib["name"]
-            ).text = tokenTexts.find("./tokenText[@name='singleVP']").text.format(
-                name=vp_piece.attrib["name"],
-                image=self._get_image_name(vp_piece.attrib["name"]) + "vp.png",
-                vp=vp_piece.attrib["VP"],
-                gpid=str(self.gpid)
-            )
+        for vp_piece in vp_node:
+            text = ""
+            match vp_piece.tag:
+                case "singleVP":
+                    text = tokenTexts.find("./tokenText[@name='singleVP']").text.format(
+                        name=vp_piece.attrib["name"],
+                        image=self._get_image_name(vp_piece.attrib["name"]) + "vp.png",
+                        vp=vp_piece.attrib["VP"],
+                        gpid=str(self.gpid)
+                    )
+                case "multiVP":
+                    max_vp = int(vp_piece.attrib["maxVP"])
+                    step = 1
+                    if "step" in vp_piece.attrib:
+                        step = int(vp_piece.attrib["step"])
+                    min_vp = self._get_min_value(step, max_vp)
+                    text = tokenTexts.find("./tokenText[@name='multiVP']").text.format(
+                        name=vp_piece.attrib["name"],
+                        step=str(step),
+                        minVP=str(min_vp),
+                        maxVP=str(max_vp),
+                        images=",".join([
+                            self._get_image_name(vp_piece.attrib["name"]) + str(i) + "vp.png" \
+                                  for i in range(
+                                      min_vp,
+                                      max_vp + 1,
+                                      step)
+                        ]),
+                        vpLevels=",".join([str(i) + " VP" \
+                            for i in range(
+                                min_vp,
+                                max_vp + 1,
+                                step)
+                        ]),
+                        gpid=str(self.gpid)
+                    )
+                case "anfa":
+                    text=tokenTexts.find("./tokenText[@name='anfa']").text
+            if "name" in vp_piece.attrib:
+                self._get_subelement(panel, vp_piece.attrib["name"], text)
+            else:
+                self._get_subelement(panel, "Anfa", text)
             self.gpid += 1
         return panel
+    
+    def _get_min_value(self, step: int, max_vp: int) -> int:
+        return step if max_vp % step == 0 else max_vp % step
 
-    def _get_subelement(self, panel_element: ET.Element, name: str) -> ET.Element:
-        return ET.SubElement(
+    def _get_subelement(self, panel_element: ET.Element, name: str, text: str) -> ET.Element:
+        element = ET.SubElement(
             panel_element,
             "VASSAL.build.widget.PieceSlot",
             attrib={
@@ -84,3 +120,6 @@ class PieceBuilder:
                 "width": "75"
             },
         )
+        if(text is not None):
+            element.text = text
+        return element
